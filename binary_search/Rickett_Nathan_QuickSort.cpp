8a9d25728869bc76a5555aa68c8ead2105af8e07
nathanrickett <nathanrickett@hotmail.com>
2021-11-01 12:31:26 -0700

    sdfsdf

--- binary_search/Rickett_Nathan_QuickSort.cpp
+++ binary_search/Rickett_Nathan_QuickSort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdio.h>
 
 using std::cout;
 using std::endl;
@@ -10,7 +11,7 @@ using std::stringstream;
 
 //reads all floating point numbers from input.txt file
 //stores floating point numbers in an array and returns it
-float* getNumbers(string aFile) {
+float * getNumbers(string aFile) {
   string line;
   string allLines;
   ifstream file (aFile);
@@ -37,55 +38,91 @@ float* getNumbers(string aFile) {
     while (s >> num) {
         array[counter] = stof(num);
         counter++;
-        cout << num + " ";
     }
+    cout << array[4];
     return array;
 }
 
 //implements quick sort algorithm. Returns sorted array
-float* quickSort(float* array) {
-  int arraySize = sizeof(array)/sizeof(array[0]);
-  float values[3];
-  values[0] = array[0];
-  values[1] = array[arraySize-1];
-  values[2] = array[(arraySize-1)/2];
-  std::sort(values, values+3);
-  float pivot = values[1];
-  return array;
+/* C implementation QuickSort */
+ 
+// A utility function to swap two elements
+void swap(float* a, float* b)
+{
+   cout << "swapping";
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
+ 
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition (float arr[], int low, int high)
+{
+  cout << "partition\n";
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+ 
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+ 
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(float arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+ 
 
-//swaps two elements within an array and returns the new array
-float* swap(float* oldArray, int positionX, int positionY) {
-  float* newArray;
-  delete(oldArray);
-  return newArray;
-}
-
-//partititions the array into a left side and returns it
-void partitionLeft(float* oldArray) {
-  float* newArray;
-  delete(oldArray);
-  return newArray;
-}
-
-//partitions the array into a rightside and returns it
-void partitionRight(float* oldArray) {
-  float* newArray;
-  delete(oldArray);
-  return newArray;
-}
 
 //Output the contents of the quicksort to a file called "output.txt"
 void WriteToFile(float* array) {
 
 }
 
+/* Function to print an array */
+void printArray(float* arr, int size)
+{
+    
+}
+
+
 int main(int argc, char *argv[]) {
   float* array;
   if (argc == 2) {
     array = getNumbers(argv[1]);
-    array = quickSort(array);
-    WriteToFile(array);
+    //int n = sizeof(array)/sizeof(array[0]);
+    //printArray(array, n);
+    //printArray(array, n);
+    //quickSort(array, 0, n-1);
+    //printf("Sorted array: \n");
+    //WriteToFile(array);
   }
   else {
     cout << "there must be exactly one file as parameter";
