7f913415f17870ca01bbf88dd12951062149b1e2
nathanrickett <nathanrickett@hotmail.com>
2021-11-01 19:19:20 -0700

    algorithm needs work

---
+++ binary_search/InputFileGenerator.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::string;
+using std::endl;
+using std::to_string;
+
+void makeFile10(int number) {
+    string file_name = "numbers/ten" + to_string(number) + ".txt";
+    std::ofstream file (file_name);
+    for(int i = 0; i < 10; ++i) {
+        float random_num = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/100));
+        file << random_num << " ";
+    }
+    file.close();
+}
+
+void makeFiles100(int number) {
+    string file_name = "numbers/one_hundred" + to_string(number) + ".txt";
+    std::ofstream file (file_name);
+    for(int i = 0; i < 100; ++i) {
+        float random_num = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/100));
+        file << random_num << " ";
+    }
+    file.close();
+}
+
+void makeFile1000(int number) {
+    string file_name = "numbers/one_thousand" + to_string(number) + ".txt";
+    std::ofstream file (file_name);
+    for(int i = 0; i < 1000; ++i) {
+        float random_num = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/100));
+        file << random_num << " ";
+    }
+    file.close();
+}
+
+void makeFile10000(int number) {
+    string file_name = "numbers/ten_thousand" + to_string(number) + ".txt";
+    std::ofstream file (file_name);
+    for(int i = 0; i < 10000; ++i) {
+        float random_num = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/100));
+        file << random_num << " ";
+    }
+    file.close();
+}
+
+void makeFile100000(int number) {
+    string file_name = "numbers/one_hundred_thousand" + to_string(number) + ".txt";
+    std::ofstream file (file_name);
+    for(int i = 0; i < 100000; ++i) {
+        float random_num = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/100));
+        file << random_num << " ";
+    }
+    file.close();
+}
+
+int main() {
+    for (int i = 1; i <= 100; ++i) {
+        makeFile10(i);
+        makeFiles100(i);
+        makeFile1000(i);
+        makeFile10000(i);
+        makeFile100000(i);
+    }
+    return 0;
+}
--- binary_search/Rickett_Nathan_QuickSort.cpp
+++ binary_search/Rickett_Nathan_QuickSort.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <sstream>
 #include <stdio.h>
+#include <algorithm>
+#include <unistd.h>
+#include <bits/stdc++.h>
 
 using std::cout;
 using std::endl;
@@ -9,9 +12,9 @@ using std::string;
 using std::ifstream;
 using std::stringstream;
 
-//reads all floating point numbers from input.txt file
-//stores floating point numbers in an array and returns it
 int arraySize;
+time_t start, end;
+
 
 float* getNumbers(string aFile) {
   string line;
@@ -56,78 +59,108 @@ void swap(float* a, float* b)
     *a = *b;
     *b = t;
 }
+
+//used for debugging
+void printArray(float* array, int size)
+{
+    for (int i = 0; i < size; ++i) {
+      cout << array[i] << " ";
+    }
+}
+
+//finds and returns the pivot point using the median of three method
+float* organize(float* array, int low, int middle, int high) {
+  float numbers[3] = {array[low], array[middle], array[high]};
+  std::sort(numbers, numbers+3); // sorting the numbers to get the true median
+  array[low] = numbers[0];
+  array[middle] = numbers[1];
+  array[high] = numbers[2];
+  return array;
+}
  
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
 int partition (float arr[], int low, int high)
 {
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
- 
-    for (int j = low; j <= high- 1; j++)
+    int middle = high/2;
+    arr = organize(arr, low, middle, high); // the pivot is now in the center
+    cout << endl << "\npivot: " << arr[middle] << endl;
+    swap(&arr[middle], &arr[high-1]); //swapping pivot to high-1
+    printArray(arr, arraySize);
+    int i = (low);  // Index of smaller element
+    for (int j = low+1; j < high-1; j++)
     {
         // If current element is smaller than or
         // equal to pivot
-        if (arr[j] <= pivot)
+        cout << endl << arr[j] << " <= " << arr[high-1] << endl;
+        if (arr[j] <= arr[high-1])  // if j <= the pivot
         {
             i++;    // increment index of smaller element
+            cout << "swapping " << arr[i] << " with " <<  arr[j];
             swap(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
+    swap(&arr[i + 1], &arr[high-1]); //swapping i with pivot
     return (i + 1);
 }
+
  
 /* The main function that implements QuickSort
  arr[] --> Array to be sorted,
   low  --> Starting index,
   high  --> Ending index */
-void quickSort(float arr[], int low, int high)
-{
+void quickSort(float arr[], int low, int high) {
+    time(&start);
     if (low < high)
     {
         /* pi is partitioning index, arr[p] is now
            at right place */
         int pi = partition(arr, low, high);
+        cout << "new patition index: " << pi << endl;
+        printArray(arr, arraySize);
  
         // Separately sort elements before
         // partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
+    time(&end);
 }
  
 
-
-//Output the contents of the quicksort to a file called "output.txt"
-void WriteToFile(float* array) {
-
+//Output the time taken for the quick sort algorithm to a text file
+void makeAverageExecution(int inputSize, double timeTaken) {
+  string file_name = "Rickett_Nathan_averageExecutionTime.txt";
+  std::ofstream file;
+  file.open(file_name, std::ios::out | std::ios::app);
+  file << endl << inputSize << "         " << timeTaken;
 }
 
-//used for debugging
-void printArray(float* array, int size)
-{
-    for (int i = 0; i < size; ++i) {
-      cout << array[i];
-      cout << "\n";
-    }
+void makeExecution(int inputSize, double timeTaken) {
+  string file_name = "Rickett_Nathan_executionTime.txt";
+  std::ofstream file;
+  file.open(file_name, std::ios::out | std::ios::app);
+  file << endl << inputSize << "         " << timeTaken;
 }
 
 
 int main(int argc, char *argv[]) {
-  if (argc == 2) {
+  /**
+   if (argc == 2) {
+    
     float* array = getNumbers(argv[1]);
-    printArray(array, arraySize);
     quickSort(array, 0, arraySize-1);
-    cout << endl;
-    printArray(array, arraySize);
-    //WriteToFile(array)
+    double time_taken = double(end - start);
   }
   else {
     cout << "there must be exactly one file as parameter";
+  }
+   * **/
+
+  for (int i = 0 ; i < 1; ++i) {
+      string file_name = "numbers/one_hundred_thousand" + std::to_string(i+1) + ".txt";
+      float* array = getNumbers(file_name);
+      quickSort(array, 0, arraySize-1);
+      double time_taken = double(end - start);
+      makeExecution(100000, time_taken);
   }
   return 0;
 }
